60c59fb536dca077e4f6884e977a321d327eb756
Magma122 <dooolooooo@gmail.com>
2024-12-28 14:28:29 +0700

    up

--- adsd.lua
+++ adsd.lua
@@ -18,7 +18,7 @@ elseif queue_on_teleport then
     ]])
 end
 
-local bestPrice = 5000
+local bestPrice = 4000
 
 function convertToNumber(text)
     local multipliers = {b= 1000000000, m = 1000000, k = 1000}
