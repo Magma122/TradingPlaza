654f782d7f87aaa924f55238bacd07859344884c
Magma122 <dooolooooo@gmail.com>
2025-01-12 01:23:56 +0700

    up

--- adsd.lua
+++ adsd.lua
@@ -81,7 +81,7 @@ for _, booth in pairs(booths:GetChildren()) do
                         itemCost.Text = item:WaitForChild("Buy"):WaitForChild("Cost").Text .. n.id
         
                         local cost = convertToNumber(item:WaitForChild("Buy"):WaitForChild("Cost").Text)
-                        local bestPrice = convertToNumber(n.BestPrice)
+                        local bestPrice = tonumber(n.BestPrice)
 
                         if cost <= bestPrice then
                             BuyItem(item, owner, cost)
