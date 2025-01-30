9f915733411060fa7fe73e4460322cf18e38ca95
Magma122 <dooolooooo@gmail.com>
2025-01-30 22:30:50 +0700

    0.0.0

--- adsd.lua
+++ adsd.lua
@@ -1,10 +1,4 @@
-if not game:IsLoaded() then
-    local notLoaded = Instance.new("Message")
-    notLoaded.Parent = game:GetService("CoreGui")
-    notLoaded.Text = "Wait"
-    game.Loaded:Wait()
-    notLoaded:Destroy()
-end
+repeat task.wait() until game:IsLoaded()
 
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
 
